7f2a0b5ac753b8f115b6dbbea499f32188cdb51c
alu0100505078 <alu0100505078@ull.edu.es>
2016-11-10 23:45:22 +0000

    Preguntas y javascripts para hacer sftp y test

--- src/start.js
+++ src/start.js
@@ -1,53 +1,57 @@
-// Copiar el contenido del servidor de express y ssubirlo al Iaas.
+// Copiar el contenido del servidor de express y subirlo al Iaas.
 
-import Client from 'ssh2-sftp-client'
+import SftpClient from 'ssh2-sftp-client'
 import path from 'path'
 import fs from 'fs'
+import SshClient from 'ssh-promise'
+import upload from './upload'
 
-async function start (dirRemoto) {
-  let sftp = new Client();
+export default async function start (config) {
+  //Funcion que realiza un sftp para subir los archivos al servidor del iaas y además realiza una conexión ssh para dejar express abierto.
+  let sftp = new SftpClient();
+
+  console.log(config)
+
   let dirLocal = path.resolve(__dirname, '../iaasserver')
 
-  try {
-    await sftp.connect({
-      host: '95.122.54.178',
+  const connectConfig = {
+      host: config.host,
       port: '22',
-      username: 'rafa',
-      password: 'pruebecita'
-  })
+      username: config.username,
+      privateKey: fs.readFileSync(config.privateKey, {encoding: 'utf8'})
+  }
 
-  await upload(sftp, dirLocal, dirRemoto)
+  const serverConfig = JSON.stringify({
+    host: config.host,
+    clientID: config.clientID,
+    clientSecret: config.clientSecret,
+    organizacion: config.organizacion
+  }, undefined, 2)
 
-  console.log('He terminado el uploaddd!!!')
+  fs.writeFileSync(path.resolve(__dirname, '../iaasserver/config.json'), serverConfig)
 
-  // await upload(sftp, dirLocal, dirRemoto)
-
-  await sftp.end()
+  try {
+    await sftp.connect(connectConfig)
+    await upload(sftp, dirLocal, config.directorioIaas)
+    console.log('He terminado de subir el fichero')
+    await sftp.end()
   } catch(err) {
-    console.log('Catcheé un error!! :(')
+    console.error('Hubo un error intentando subir los ficheros')
     await sftp.end()
     throw err
   }
-}
 
-async function upload (sftp, pathLocal, pathRemoto) {
-  // Funcion que actualiza el servidor remoto con los archivos locales.
-  process.stdout.write(`${pathLocal} -> ${pathRemoto}`)
+  try {
+    const ssh = new SshClient(connectConfig);
 
-  if (fs.lstatSync(path.join(pathLocal)).isDirectory()) {
-    console.log(' Creando directorio...')
-    await sftp.mkdir(pathRemoto)
-    let files = fs.readdirSync(pathLocal)
-    for (let file of files) {
-      await upload(sftp, path.join(pathLocal, file), path.join(pathRemoto, file))
-    }
-    return
+    const commands = [
+      `cd ${config.directorioIaas} && npm i && screen -d -m npm start` // screen -RR y nos reconectamos al servidor que se quedó abierto
+    ]
+
+    await ssh.exec(commands)
+
+  } catch(err) {
+    console.error('Hubo un errror!!')
+    throw err
   }
-
-  await sftp.put(path.resolve(pathLocal), path.resolve(pathRemoto))
-  console.log(' OK!')
 }
-
-// Pruebas!!
-// start('/home/rafa/proyecto').then(() => console.log('He terminado de hacer todo')).catch(console.error)
-
